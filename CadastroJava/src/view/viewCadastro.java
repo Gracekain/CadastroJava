8d2f75a63297ba15da1ba34265ba2dbf17f61a85
JiiJ eu ae <jonas_g_pereira@estudante.sc.senai.br>
2017-10-10 21:59:40 -0300

    Preenchimento da combobox de categorias

--- CadastroJava/src/view/viewCadastro.java
+++ CadastroJava/src/view/viewCadastro.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import model.bean.Categoria;
+import model.dao.CategoriaDAO;
+
 /**
  *
  * @author Administrador
@@ -13,9 +16,18 @@ public class viewCadastro extends javax.swing.JFrame {
 
     /**
      * Creates new form viewCadastro
+     * Método Construtor
      */
     public viewCadastro() {
         initComponents();
+        preencherComboboxCategorias();
+    }
+    
+    private void preencherComboboxCategorias(){
+        CategoriaDAO catDao = new CategoriaDAO();
+        for (Categoria cat:catDao.findAll()) {
+            jComboBoxCategorias.addItem(cat);
+        }
     }
 
     /**

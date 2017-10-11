3fef1b1f0a9f6db26e6364d5ca3e21e07e15a604
Jonas <jonas_g_pereira@estudante.sc.senai.br>
2017-10-11 20:28:34 -0300

    Tabela de produtos preenchida - closes #1

--- CadastroJava/src/view/viewCadastro.java
+++ CadastroJava/src/view/viewCadastro.java
@@ -5,8 +5,11 @@
  */
 package view;
 
+import javax.swing.table.DefaultTableModel;
 import model.bean.Categoria;
+import model.bean.Produto;
 import model.dao.CategoriaDAO;
+import model.dao.ProdutoDAO;
 
 /**
  *
@@ -14,6 +17,7 @@ import model.dao.CategoriaDAO;
  */
 public class viewCadastro extends javax.swing.JFrame {
 
+        private DefaultTableModel dtmProdutos;
     /**
      * Creates new form viewCadastro
      * Método Construtor
@@ -21,6 +25,27 @@ public class viewCadastro extends javax.swing.JFrame {
     public viewCadastro() {
         initComponents();
         preencherComboboxCategorias();
+        dtmProdutos = (DefaultTableModel) jTableProdutos.getModel();
+        preencherTabelaProdutos();
+    }
+    
+    private void preencherTabelaProdutos(){
+        ProdutoDAO pDao = new ProdutoDAO();
+        int idProduto = 0;
+        String descProduto = "";
+        int quantidade = 0;
+        double valor = 0.0d;
+        int idCategoria = 0;
+        dtmProdutos.setRowCount(0); // Reseta a tabela (limpa)
+        for(Produto p : pDao.findAll()){
+            idProduto = p.getIdProduto();
+            descProduto = p.getDescricao();
+            quantidade = p.getQtd();
+            valor = p.getValor();
+            idCategoria = p.getCategoria().getIdCategoria(); //Composição Objeto dentro de Objeto
+            Object[] dados = {idProduto, descProduto, quantidade, valor, idCategoria};
+            dtmProdutos.addRow(dados);
+        }
     }
     
     private void preencherComboboxCategorias(){
